5abb917b5405ea87c3167d453724633737f77f62
Rayssa Maely <rayssamaely0306@gmail.com>
2025-11-14 21:15:09 -0300

    Alterações no dashboard.

--- MauiAppMinhaPrevisao/Views/Dashboard.xaml.cs
+++ MauiAppMinhaPrevisao/Views/Dashboard.xaml.cs
@@ -6,4 +6,19 @@ public partial class Dashboard : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_2(object sender, EventArgs e)
+    {
+
+    }
 }
